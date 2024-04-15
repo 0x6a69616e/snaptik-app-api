8b9bf29422260405f15ca9566b5308a6e2cdd53e
0x6a69616e <0x6a69616e@gmail.com>
2024-04-15 09:11:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,98 @@
-const
-  axios = require('axios'),
-  cheerio = require('cheerio'),
-  FormData = require('form-data');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const FormData = require('form-data');
 
-module.exports = class {
+class SnapTikClient {
   constructor(config = {}) {
-    function randomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    this.axios = axios.create(this.config = Object.assign({
-      baseURL: 'https://snaptik.app',
-      headers: {
-        'User-Agent': `Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.${randomInt(0, 9999)}.${randomInt(0, 99)} Safari/537.36`
-      }
-    }, config));
+    this.axios = axios.create(this.config = {
+      baseURL: 'https://dev.snaptik.app',
+      ...config,
+    });
   }
 
   async get_token() {
     const {
       data
-    } = await this.axios.get('/');
-    return cheerio.load(data)('input[name="token"]').val();
+    } = await this.axios({
+      url: '/'
+    });
+    const $ = cheerio.load(data);
+    return $('input[name="token"]').val();
   }
 
-  eval_script(script) {
-    const
-      $ = cheerio.load('<div id="hero"></div><div id="download"></div><div class="contents"></div>'),
-      args = Function('$', `return{$,document:{getElementById:a=>({src:''})},gtag:a=>0,Math:{round:a=>0},window:{location:{hostname:'snaptik.app'}},XMLHttpRequest:Function('this.open=this.send=a=>0')}`)($);
+  async get_script(url) {
+    const form = new FormData();
+    const token = await this.get_token();
 
-    $.prototype.style = {};
-    Object.defineProperty($.prototype, 'innerHTML', {
-      get: Function('return this.html()'),
-      set: Function('a', 'this.html(a)')
+    form.append('token', token);
+    form.append('url', url);
+
+    const {
+      data
+    } = await this.axios({
+      url: '/abc2.php',
+      method: 'POST',
+      data: form
     });
 
-    const video_url = Function(...Object.keys(args), `let fetch=a=>{fetch=a;return{json:a=>({thumbnail_url:''})}};${script};return fetch;`)(...Object.values(args));
-
-    return [
-      $('#download').html(),
-      video_url
-    ]
+    return data;
   }
 
-  parse_html(html) {
-    const
-      $ = cheerio.load(html);
-
-    return [
-      $('div.video-links > button[data-tokenhd]').data('tokenhd'),
-      $('div.video-links > a:not(a[href="/"])').toArray().map(elem => $(elem).attr('href')).map(x => x.startsWith('/') ? this.config.baseURL + x : x)
-    ]
+  async eval_script(script1) {
+    const script2 = await new Promise(resolve => Function('eval', script1)(resolve));
+    return new Promise((resolve, reject) => {
+      let html = '';
+      const [
+        k,
+        v
+      ] = [
+        'keys',
+        'values'
+      ].map(x => Object[x]({
+        $: () => Object.defineProperty({
+          remove() {},
+          style: {
+            display: ''
+          }
+        }, 'innerHTML', {
+          set: t => (html = t)
+        }),
+        app: {
+          showAlert: reject
+        },
+        document: {
+          getElementById: () => ({
+            src: ''
+          })
+        },
+        fetch: a => {
+          return resolve({
+            html,
+            oembed_url: a
+          }), {
+            json: () => ({
+              thumbnail_url: ''
+            })
+          };
+        },
+        gtag: () => 0,
+        Math: {
+          round: () => 0
+        },
+        XMLHttpRequest: function() {
+          return {
+            open() {},
+            send() {}
+          }
+        },
+        window: {
+          location: {
+            hostname: 'snaptik.app'
+          }
+        }
+      }));
+      Function(...k, script2)(...v);
+    });
   }
 
   async get_hd_video(token) {
@@ -59,79 +101,65 @@ module.exports = class {
         error,
         url
       }
-    } = await this.axios.get('/getHdLink.php?token=' + token);
+    } = await this.axios({
+      url: '/getHdLink.php?token=' + token
+    });
 
     if (error) throw new Error(error);
     return url;
   }
 
-  parse_oembed_html(html) {
-    const
-      $ = cheerio.load(html),
-      music = $('section > a:last-child'),
-      video = $('blockquote');
+  async parse_html(html) {
+    const $ = cheerio.load(html);
+    const is_video = !$('div.render-wrapper').length;
 
-    return {
-      title: $('p').contents().filter(Function('return this.nodeType === 3')).text().trim(),
-      tags: $('p > a').toArray().map(elem => [$(elem).attr('title'), $(elem).attr('href')]),
-      music_name: music.attr('title'),
-      music_url: music.attr('href'),
-      video_url: video.attr('cite'),
-      video_id: video.data('video-id')
+    return is_video ? (async () => {
+      const hd_token = $('div.video-links > button[data-tokenhd]').data('tokenhd');
+      const hd_url = new URL(await this.get_hd_video(hd_token));
+      const token = hd_url.searchParams.get('token');
+      const raw_hd_url = JSON.parse(atob(token.split('.')[1].replace(/-/g, '+').replace(/_/g, '/'))).url;
+
+      return {
+        type: 'video',
+        data: {
+          sources: [
+            raw_hd_url,
+            hd_url.href,
+            ...$('div.video-links > a:not(a[href="/"])')
+            .toArray()
+            .map(elem => $(elem).attr('href'))
+            .map(x => x.startsWith('/') ? this.config.baseURL + x : x)
+          ]
+        }
+      }
+    })() : {
+      type: 'slideshow',
+      data: {
+        photos: $('div.columns > div.column > div.photo').toArray().map(elem => ({
+          sources: [
+            $(elem).find('img').attr('src'),
+            $(elem).find('a').attr('href')
+          ]
+        }))
+      }
     }
   }
 
   async process(url) {
-    const
-      token = await this.get_token(),
-      form = new FormData();
-
-    this.token = token;
-
-    form.append('token', token);
-    form.append('url', url);
-
-    function atob(data) {
-      return Buffer.from(data, 'base64').toString('binary');
-    }
+    const script = await this.get_script(url);
 
     const {
-      data
-    } = await this.axios.post('/abc2.php', form),
-      [
-        globals,
-        fn
-      ] = data.match(/(.*)eval\((function.*)\)/).slice(1),
-      script = Function(`${globals}return (${fn})`)(),
-      [
-        html,
-        oembed_url
-      ] = this.eval_script(script),
-      [
-        _token,
-        srcs
-      ] = this.parse_html(html), {
-        id
-      } = JSON.parse(atob(_token.split('.')[1]));
+      html,
+      oembed_url
+    } = await this.eval_script(script);
 
     return {
-      sources: [
-        async () => await this.get_hd_video(_token),
-        ...srcs
-      ],
-      oembed: {
-        url: oembed_url,
-        get_data: async () => {
-          const {
-            data
-          } = await this.axios.get('/oembed?url=https://www.tiktok.com/@tiktok/video/' + id, {
-            baseURL: 'https://www.tiktok.com'
-          });
-          data['_html'] = this.parse_oembed_html(data.html);
-          return data
-        }
-      },
-      video_id: id
-    }
+      ...(await this.parse_html(html)),
+      url,
+      oembed_url,
+      script
+    };
   }
 }
+
+module.exports = SnapTikClient;
